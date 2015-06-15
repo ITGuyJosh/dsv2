deaf2d6f420e0edc1f4944aaf8f4c7b1a9c7c0ee
Josh Evans <joshuaevans@irs-limited.com>
2015-06-15 14:27:39 +0100

    basic group document auth checking

--- app/Controller/GroupDocumentsController.php
+++ app/Controller/GroupDocumentsController.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppController', 'Controller');
+
 /**
  * GroupDocuments Controller
  *
@@ -8,107 +10,117 @@ App::uses('AppController', 'Controller');
  */
 class GroupDocumentsController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Paginator');
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator');
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->GroupDocument->recursive = 0;
-		$this->set('groupDocuments', $this->Paginator->paginate());
-	}
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index() {
+        $this->GroupDocument->recursive = 0;
+        $this->set('groupDocuments', $this->Paginator->paginate());
+    }
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->GroupDocument->exists($id)) {
-			throw new NotFoundException(__('Invalid group document'));
-		}
-		$options = array('conditions' => array('GroupDocument.' . $this->GroupDocument->primaryKey => $id));
-		$this->set('groupDocument', $this->GroupDocument->find('first', $options));
-	}
+    /**
+     * view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null) {
+        if (!$this->GroupDocument->exists($id)) {
+            throw new NotFoundException(__('Invalid group document'));
+        }
+        $options = array('conditions' => array('GroupDocument.' . $this->GroupDocument->primaryKey => $id));
+        $this->set('groupDocument', $this->GroupDocument->find('first', $options));
+    }
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->GroupDocument->create();
-			if ($this->GroupDocument->save($this->request->data)) {
-				$this->Session->setFlash(__('The group document has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The group document could not be saved. Please, try again.'));
-			}
-		}
-		$groups = $this->GroupDocument->Group->find('list');
-		$this->set(compact('groups'));
-	}
-
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->GroupDocument->exists($id)) {
-			throw new NotFoundException(__('Invalid group document'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->GroupDocument->save($this->request->data)) {
-				$this->Session->setFlash(__('The group document has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The group document could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('GroupDocument.' . $this->GroupDocument->primaryKey => $id));
-			$this->request->data = $this->GroupDocument->find('first', $options);
-		}
-		$groups = $this->GroupDocument->Group->find('list');
-		$this->set(compact('groups'));
-	}
-
-/**
- * delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		$this->GroupDocument->id = $id;
-		if (!$this->GroupDocument->exists()) {
-			throw new NotFoundException(__('Invalid group document'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->GroupDocument->delete()) {
-			$this->Session->setFlash(__('The group document has been deleted.'));
-		} else {
-			$this->Session->setFlash(__('The group document could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
-        
-        public function ugroup($uid) {            
-            if($this->Auth->user("id") == $uid){
-                
+    /**
+     * add method
+     *
+     * @return void
+     */
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->GroupDocument->create();
+            if ($this->GroupDocument->save($this->request->data)) {
+                $this->Session->setFlash(__('The group document has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The group document could not be saved. Please, try again.'));
             }
-	}
+        }
+        $groups = $this->GroupDocument->Group->find('list');
+        $this->set(compact('groups'));
+    }
+
+    /**
+     * edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null) {
+        if (!$this->GroupDocument->exists($id)) {
+            throw new NotFoundException(__('Invalid group document'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->GroupDocument->save($this->request->data)) {
+                $this->Session->setFlash(__('The group document has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The group document could not be saved. Please, try again.'));
+            }
+        } else {
+            $options = array('conditions' => array('GroupDocument.' . $this->GroupDocument->primaryKey => $id));
+            $this->request->data = $this->GroupDocument->find('first', $options);
+        }
+        $groups = $this->GroupDocument->Group->find('list');
+        $this->set(compact('groups'));
+    }
+
+    /**
+     * delete method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null) {
+        $this->GroupDocument->id = $id;
+        if (!$this->GroupDocument->exists()) {
+            throw new NotFoundException(__('Invalid group document'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->GroupDocument->delete()) {
+            $this->Session->setFlash(__('The group document has been deleted.'));
+        } else {
+            $this->Session->setFlash(__('The group document could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
+
+    public function ugroup($uid) {
+
+        if (empty($uid)) {
+            $this->Session->setFlash(__('You must be logged in to access that section.'));
+            return $this->redirect(array("controller" => "users", 'action' => 'login'));
+        }
+
+        if ($this->Auth->user("id") !== $uid) {
+            $this->Session->setFlash(__('You do not have appropriate credentials to access that section.'));
+            return $this->redirect(array("controller" => "users", 'action' => 'login'));
+        }
+        
+        
+    }
+
 }
